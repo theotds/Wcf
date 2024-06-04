599761ea8bcf28e0777632e2dd2ba63c07dcf84d
theotds <theo.deschaeck@gmail.com>
2024-06-04 22:03:30 +0200

    first commit

---
+++ Wcf.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wcf.Client
+{
+    internal class Program
+    {
+        [ServiceContract]
+        public interface IMessageService
+        {
+            [OperationContract]
+            string[] GetMessages();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("press any key to continue...");
+            Console.ReadLine();
+            string uri = "net.tcp://localhost:6565/MessageService";
+            NetTcpBinding binding = new NetTcpBinding(SecurityMode.None);
+            var channel = new ChannelFactory<IMessageService>(binding);
+            var endpoint = new EndpointAddress(uri);
+            var proxy = channel.CreateChannel(endpoint);
+            var result = proxy?.GetMessages();
+            if(result != null) { 
+                result.ToList().ForEach(x => Console.WriteLine(x));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Wcf.Server/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wcf.Server
+{
+    internal class Program
+    {
+        [ServiceContract]
+        public interface IMessageService
+        {
+            [OperationContract]
+            string[] GetMessages();
+        }
+
+        [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+        public class MessageService : IMessageService
+        {
+            public string[] GetMessages()
+            {
+                return new string[] { "server1", "server2", "server3" };
+            }
+        }
+        static void Main(string[] args)
+        {
+            var uris = new Uri[1];
+            string address = "net.tcp://localhost:6565/MessageService";
+            uris[0] = new Uri(address);
+            IMessageService message = new MessageService();
+            ServiceHost host = new ServiceHost(message, uris);
+            var binding = new NetTcpBinding(SecurityMode.None);
+            host.AddServiceEndpoint(typeof(IMessageService), binding, "");
+            host.Opened += Host_Opened;
+            host.Open();
+            Console.ReadLine();
+        }
+
+        private static void Host_Opened(object sender, EventArgs e)
+        {
+            Console.WriteLine("message service started");
+        }
+      
+    }
+}
